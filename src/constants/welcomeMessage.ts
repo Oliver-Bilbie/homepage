c398aebf339c497cc0b08e7faab74b7d8be2570d
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2023-02-13 21:47:29 +0000

    Test

--- src/constants/welcomeMessage.ts
+++ src/constants/welcomeMessage.ts
@@ -1,5 +1,5 @@
 export const WELCOME_MESSAGE = [
-    "This webpage is a showcase of projects that I have developed in my spare time. Each project has a brief description along with links to the source code and the application itself. Further information may be found in a project's README file within the source code.",
-    "In addition to these projects, the infrastucture-as-code for the CI/CD pipelines used to deploy the projects can also be found on my GitHub page, along with the source code for this page, and some older projects which are no longer maintained.",
-    "You can use the arrow to the right of this text or the selectors below to navigate to the next page.",
+  "This webpage is a showcase of projects that I have developed in my spare time. Each project has a brief description along with links to the source code and the application itself. Further information may be found in a project's README file within the source code.",
+  "In addition to these projects, the infrastucture-as-code for the CI/CD pipelines used to deploy the projects can also be found on my GitHub page, along with the source code for this page, and some older projects which are no longer maintained.",
+  "You can use the arrow to the right of this text or the selectors below to navigate to the next page.",
 ];
